10e491d64d571a73674ba46240e6795e36897dfb
Ivan Torres de Oses <ivantorresoses2@gmail.com>
2019-11-04 09:43:45 +0100

    modificado ejercicio 15

--- Tema6/Ejercicio15.java
+++ Tema6/Ejercicio15.java
@@ -8,43 +8,81 @@ public class Ejercicio15 {
     
     int notaAct;
     int cantidadNotas;
+    int primNot = 1;
     do {
       
       cantidadNotas = (int) (Math.random() * 24 + 4);
     } while (cantidadNotas % 4 != 0);
     for (int i = 1; i <= cantidadNotas; i++) {
-      
-      notaAct = (int) (Math.random() * 7 +1);
-      switch (notaAct) {
+        if (i != cantidadNotas) {
+        notaAct = (int) (Math.random() * 7 +1);
+        if (i == 1) {
+          primNot = notaAct;
+        }
+        switch (notaAct) {
+          
+          case 1:
+            
+            System.out.print("do ");
+            break;
+          case 2:
+            
+            System.out.print("re ");
+            break;
+          case 3:
+            
+            System.out.print("mi ");
+            break;
+          case 4:
+            
+            System.out.print("fa ");
+            break;
+          case 5:
+            
+            System.out.print("sol ");
+            break;
+          case 6:
+            
+            System.out.print("la ");
+            break;
+          case 7:
+            
+            System.out.print("si ");
+            break;
+        }
+      } else {
         
-        case 1:
+        switch (primNot) {
           
-          System.out.print("do ");
-          break;
-        case 2:
-          
-          System.out.print("re ");
-          break;
-        case 3:
-          
-          System.out.print("mi ");
-          break;
-        case 4:
-          
-          System.out.print("fa ");
-          break;
-        case 5:
-          
-          System.out.print("sol ");
-          break;
-        case 6:
-          
-          System.out.print("la ");
-          break;
-        case 7:
-          
-          System.out.print("si ");
-          break;
+          case 1:
+            
+            System.out.print("do ");
+            break;
+          case 2:
+            
+            System.out.print("re ");
+            break;
+          case 3:
+            
+            System.out.print("mi ");
+            break;
+          case 4:
+            
+            System.out.print("fa ");
+            break;
+          case 5:
+            
+            System.out.print("sol ");
+            break;
+          case 6:
+            
+            System.out.print("la ");
+            break;
+          case 7:
+            
+            System.out.print("si ");
+            break;
+        }
       }
       if (i % 4 == 0) {
         System.out.print(" | ");
