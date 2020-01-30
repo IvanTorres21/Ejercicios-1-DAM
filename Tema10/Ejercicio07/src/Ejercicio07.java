e87f4257dfcb8182516acc263c9267e2112ebd9b
IvanTorres21 <ivantorresoses2@gmail.com>
2020-01-30 08:34:37 +0100

    Modificado ejercicio 7 tema 10 para que no se repitan los valores tanto

--- Tema10/Ejercicio07/src/Ejercicio07.java
+++ Tema10/Ejercicio07/src/Ejercicio07.java
@@ -1,12 +1,13 @@
 import java.util.ArrayList;
 
 public class Ejercicio07 {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws InterruptedException{
     
     ArrayList<Moneda> monedas = new ArrayList<Moneda>();
     monedas.add(new Moneda());
     for (int i = 1; i < 6; i++) {
       
+      Thread.sleep(500); // Si no le digo que se espere un poco lo hace tan rápido que todos son casi iguales
       monedas.add(new Moneda(monedas.get(i-1)));
     }
     for (Moneda m : monedas) {
